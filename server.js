d697d792965fde730ee6b4810af721ee48cfb7bc
Micha Alt <mail@lean-stack.de>
2017-07-24 22:18:21 +0200

    npm dependency tingo-rest installed.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+const tingoRest = require('tingo-rest');
+app.use('/api',tingoRest(__dirname + '/data'));
+
 app.use(express.static('dist'));
 
 app.listen(2121, function () {
